6ad8d394ae3a1a65b4f1dacab5421601d7da9e7e
Cyanos09 <deftera.monday@gmail.com>
2021-12-15 14:50:10 +0900

    link added and checkbox implemented

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # maliciousearch
-Chrome extension to search for hashes of malware.
+Chrome extension to search for hashes of suspicious file.
 
-Get the malware sample information from VirusTotal and get the sample link from MalShare.
+Get the suspicious file information from the following site.
 
 ## Search Engine
 VirusTotal : https://www.virustotal.com/gui/home/search
+
+JoeSandbox : https://www.joesandbox.com/
+
+HybridAnalysis : https://www.hybrid-analysis.com/
+
+yoroi : https://yomi.yoroi.company/
 
 MalShare : https://malshare.com/
--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-main();
-
 function main(){
+	document.addEventListener("DOMContentLoaded",function(){
+		var element;
+		var btn = document.getElementById('btn');
+		btn.addEventListener('click', function(){
+			var hash = document.hashform.hashtext.value;
+			var url_vt = "https://www.virustotal.com/gui/file/" + hash;
+			var url_jsb = "https://www.joesandbox.com/search?q=" + hash;
+			var url_ha = "https://www.hybrid-analysis.com/sample/" + hash;
+			var url_yoroi = "https://yomi.yoroi.company/submissions/" + hash;
+			var url_ms = "https://malshare.com/search.php?query=" + hash;
 
-	var btn = document.getElementById('btn');
-
-	btn.addEventListener('click', function(){
-		var hash = document.hashform.hashtext.value;
-		var url_ms = "https://malshare.com/search.php?query=" + hash;
-		var url_vt = "https://www.virustotal.com/gui/file/" + hash;
-		chrome.tabs.create({url: url_vt}, tab => {});
-		chrome.tabs.create({url: url_ms}, tab => {});
+			const url_btn_tuple_array = [[url_vt,'btn_vt'],[url_jsb,'btn_jsb'],[url_ha,'btn_ha'],[url_yoroi,'btn_yoroi'],[url_ms,'btn_ms']];
+			for(const url_btn_tuple of url_btn_tuple_array){
+				element = document.getElementById(url_btn_tuple[1]).checked;
+				if(element){
+					chrome.tabs.create({url: url_btn_tuple[0]}, tab => {});
+				}
+			}
+		});
 	});
 }
+
+main();
